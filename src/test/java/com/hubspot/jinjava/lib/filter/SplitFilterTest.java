291d974529e393fee24dc14915fb5770ad96e17a
Jared Stehler <jstehler@hubspot.com>
2014-10-24 09:30:17 -0400

    adding string split filter

---
+++ src/test/java/com/hubspot/jinjava/lib/filter/SplitFilterTest.java
@@ -0,0 +1,46 @@
+package com.hubspot.jinjava.lib.filter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+
+
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("unchecked")
+public class SplitFilterTest {
+
+  @Mock JinjavaInterpreter interpreter;
+  SplitFilter filter;
+  
+  @Before
+  public void setup() {
+    filter = new SplitFilter();
+  }
+  
+  @Test
+  public void itDefaultsToSpaceSep() {
+    List<String> result = (List<String>) filter.filter("hello world  this is fred", interpreter);
+    assertThat(result).containsExactly("hello", "world", "this", "is", "fred");
+  }
+
+  @Test
+  public void itUsesDifferentSeparatorIfSpecified() {
+    List<String> result = (List<String>) filter.filter("hello world,  this is fred", interpreter, ",");
+    assertThat(result).containsExactly("hello world", "this is fred");
+  }
+  
+  @Test
+  public void itLimitsResultIfSpecified() {
+    List<String> result = (List<String>) filter.filter("hello world  this is fred", interpreter, " ", "2");
+    assertThat(result).containsExactly("hello", "world  this is fred");
+  }
+  
+}
