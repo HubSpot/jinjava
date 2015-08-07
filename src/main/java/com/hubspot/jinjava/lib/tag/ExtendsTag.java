8db074fda3f1264d897d82fa307702146596033c
Meghan Nelson <mnelson@hubspot.com>
2015-08-07 14:01:10 -0400

    Change templates to extension_resources

--- src/main/java/com/hubspot/jinjava/lib/tag/ExtendsTag.java
+++ src/main/java/com/hubspot/jinjava/lib/tag/ExtendsTag.java
@@ -89,7 +89,7 @@ public class ExtendsTag implements Tag {
       String template = interpreter.getResource(templateFile);
       Node node = interpreter.parse(template);
       JinjavaInterpreter child = new JinjavaInterpreter(interpreter);
-      child.getContext().addDependency("templates", templateFile);
+      child.getContext().addDependency("extension_resources", templateFile);
       interpreter.addExtendParentRoot(node);
       return "";
     } catch (IOException e) {
