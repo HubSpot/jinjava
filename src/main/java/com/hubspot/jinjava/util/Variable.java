9e4a65578e2afc49889d0e87829bfc67a78bf1cc
Jared Stehler <jstehler@hubspot.com>
2015-08-31 18:16:07 -0400

    code cleanup; make fields final

--- src/main/java/com/hubspot/jinjava/util/Variable.java
+++ src/main/java/com/hubspot/jinjava/util/Variable.java
@@ -1,17 +1,17 @@
 /**********************************************************************
-Copyright (c) 2014 HubSpot Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ * Copyright (c) 2014 HubSpot Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  **********************************************************************/
 package com.hubspot.jinjava.util;
 
@@ -25,26 +25,22 @@ import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 public class Variable {
   private static final Splitter DOT_SPLITTER = Splitter.on('.');
 
-  private JinjavaInterpreter interpreter;
+  private final JinjavaInterpreter interpreter;
 
-  private String name;
-  private List<String> chainList;
+  private final String name;
+  private final List<String> chainList;
 
   public Variable(JinjavaInterpreter interpreter, String variable) {
     this.interpreter = interpreter;
-    split(variable);
-  }
 
-  private void split(String variable) {
-    if (!variable.contains(".")) {
+    if (variable.indexOf('.') == -1) {
       name = variable;
       chainList = Collections.emptyList();
-      return;
+    } else {
+      List<String> parts = Lists.newArrayList(DOT_SPLITTER.split(variable));
+      name = parts.get(0);
+      chainList = parts.subList(1, parts.size());
     }
-
-    List<String> parts = Lists.newArrayList(DOT_SPLITTER.split(variable));
-    name = parts.get(0);
-    chainList = parts.subList(1, parts.size());
   }
 
   public String getName() {
