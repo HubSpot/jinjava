bb0869e9d162c26806f33711f26c39dda14b29d8
Jared Stehler <jstehler@hubspot.com>
2015-09-15 14:27:28 -0400

    trailing whitespace [ci skip]

--- src/main/java/com/hubspot/jinjava/objects/PyWrapper.java
+++ src/main/java/com/hubspot/jinjava/objects/PyWrapper.java
@@ -2,7 +2,7 @@ package com.hubspot.jinjava.objects;
 
 /**
  * Marker indicating an object is a python wrapper interface.
- * 
+ *
  * Use this on classes which you don't want automatically wrapped into Py* forms (i.e. List types into PyList)
  *
  * @author jstehler
