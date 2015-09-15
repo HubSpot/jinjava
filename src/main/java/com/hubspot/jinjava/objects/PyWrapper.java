ef484457c09a23fa86a6758ef13f0fb2dbc53572
Jared Stehler <jstehler@hubspot.com>
2015-09-15 14:20:40 -0400

    clarify pywrapper use

--- src/main/java/com/hubspot/jinjava/objects/PyWrapper.java
+++ src/main/java/com/hubspot/jinjava/objects/PyWrapper.java
@@ -1,7 +1,9 @@
 package com.hubspot.jinjava.objects;
 
 /**
- * Marker indicating an object is a python wrapper interface
+ * Marker indicating an object is a python wrapper interface.
+ * 
+ * Use this on classes which you don't want automatically wrapped into Py* forms (i.e. List types into PyList)
  *
  * @author jstehler
  */
