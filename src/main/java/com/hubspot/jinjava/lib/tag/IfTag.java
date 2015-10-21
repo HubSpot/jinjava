e12ef642ae1531395b3f13aa252fa0ebe6c853ef
Jared Stehler <jstehler@hubspot.com>
2015-10-21 13:29:11 -0400

    missing expression for if tag should be syntax error

--- src/main/java/com/hubspot/jinjava/lib/tag/IfTag.java
+++ src/main/java/com/hubspot/jinjava/lib/tag/IfTag.java
@@ -1,17 +1,17 @@
 /**********************************************************************
-Copyright (c) 2014 HubSpot Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ * Copyright (c) 2014 HubSpot Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  **********************************************************************/
 package com.hubspot.jinjava.lib.tag;
 
@@ -21,8 +21,8 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
-import com.hubspot.jinjava.interpret.InterpretException;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.interpret.TemplateSyntaxException;
 import com.hubspot.jinjava.tree.Node;
 import com.hubspot.jinjava.tree.TagNode;
 import com.hubspot.jinjava.util.ObjectTruthValue;
@@ -33,8 +33,7 @@ import com.hubspot.jinjava.util.ObjectTruthValue;
         @JinjavaSnippet(
             code = "{% if condition %}\n" +
                 "If the condition is true print this to template.\n" +
-                "{% endif %}"
-        ),
+                "{% endif %}"),
         @JinjavaSnippet(
             code = "{% if number <= 2 %}\n" +
                 "Varible named number is less than or equal to 2.\n" +
@@ -55,7 +54,7 @@ public class IfTag implements Tag {
   @Override
   public String interpret(TagNode tagNode, JinjavaInterpreter interpreter) {
     if (StringUtils.isBlank(tagNode.getHelpers())) {
-      throw new InterpretException("Tag 'if' expects expression", tagNode.getLineNumber());
+      throw new TemplateSyntaxException(tagNode.getMaster().getImage(), "Tag 'if' expects expression", tagNode.getLineNumber());
     }
 
     Iterator<Node> nodeIterator = tagNode.getChildren().iterator();
