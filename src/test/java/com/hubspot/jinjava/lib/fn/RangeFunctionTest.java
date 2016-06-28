8f0dcbc6f580cef000c6e07870f9d77d866fe0a0
Jeff Boulter <jboulter@hubspot.com>
2016-06-28 11:35:58 -0400

    test wasn't being run

--- src/test/java/com/hubspot/jinjava/lib/fn/RangeFunctionTest.java
+++ src/test/java/com/hubspot/jinjava/lib/fn/RangeFunctionTest.java
@@ -3,6 +3,7 @@ package com.hubspot.jinjava.lib.fn;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.Test;
 
@@ -10,7 +11,7 @@ public class RangeFunctionTest {
 
   @Test
   public void itGeneratesSimpleRanges() {
-    assertThat(Functions.range(1)).isEqualTo(Arrays.asList(0));
+    assertThat(Functions.range(1)).isEqualTo(Collections.singletonList(0));
     assertThat(Functions.range(2)).isEqualTo(Arrays.asList(0, 1));
     assertThat(Functions.range(2, 4)).isEqualTo(Arrays.asList(2, 3));
     assertThat(Functions.range("2", "4")).isEqualTo(Arrays.asList(2, 3));
@@ -34,10 +35,10 @@ public class RangeFunctionTest {
     assertThat(Functions.range(2, 2000, -5)).isEmpty();
   }
 
+  @Test
   public void itHandlesBadValues() {
     assertThat(Functions.range(2, "f")).isEmpty();
   }
-
 
   @Test
   public void itTruncatesHugeRanges() {
