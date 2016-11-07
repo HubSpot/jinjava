d6d88d488721fd8e2d14133d9f4f9385b82bb907
Jeff Boulter <jboulter@hubspot.com>
2016-11-07 17:30:23 -0500

    tweak docs

--- src/main/java/com/hubspot/jinjava/lib/filter/UnixTimestampFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/UnixTimestampFilter.java
@@ -7,12 +7,12 @@ import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.lib.fn.Functions;
 
 @JinjavaDoc(
-    value = "gets the unix timestamp value (in milliseconds) of a date object",
+    value = "Gets the UNIX timestamp value (in milliseconds) of a date object",
     params = {
         @JinjavaParam(value = "value", defaultValue = "current time", desc = "The date variable"),
     },
     snippets = {
-        @JinjavaSnippet(code = "{% local_dt|unixtimestamp"),
+        @JinjavaSnippet(code = "{% mydatetime|unixtimestamp %}"),
     })
 public class UnixTimestampFilter implements Filter {
 
