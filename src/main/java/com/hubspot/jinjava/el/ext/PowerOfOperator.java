64c87097836b388315235e75a5741e6c40a92603
Jeff Boulter <jboulter@hubspot.com>
2016-11-22 17:39:45 -0500

    check for nulls when building exception string

--- src/main/java/com/hubspot/jinjava/el/ext/PowerOfOperator.java
+++ src/main/java/com/hubspot/jinjava/el/ext/PowerOfOperator.java
@@ -12,24 +12,28 @@ public class PowerOfOperator extends SimpleOperator {
 
   @Override
   protected Object apply(TypeConverter converter, Object a, Object b) {
-      boolean aInt = a instanceof Integer || a instanceof Long;
-      boolean bInt = b instanceof Integer || b instanceof Long;
-      boolean aNum = aInt || a instanceof Double || a instanceof Float;
-      boolean bNum = bInt || b instanceof Double || b instanceof Float;
+    boolean aInt = a instanceof Integer || a instanceof Long;
+    boolean bInt = b instanceof Integer || b instanceof Long;
+    boolean aNum = aInt || a instanceof Double || a instanceof Float;
+    boolean bNum = bInt || b instanceof Double || b instanceof Float;
 
-      if (aInt && bInt) {
-          Long d = converter.convert(a, Long.class);
-          Long e = converter.convert(b, Long.class);
-          return (long)Math.pow(d, e);
-   }
-      if (aNum && bNum) {
-          Double d = converter.convert(a, Double.class);
-          Double e = converter.convert(b, Double.class);
-          return Math.pow(d, e);
-      }
-      throw new IllegalArgumentException("Unsupported operand type(s) for **: "
-              + "'" + a.getClass().getSimpleName() + "' and "
-              + "'" + b.getClass().getSimpleName() + "'");
+    if (aInt && bInt) {
+      Long d = converter.convert(a, Long.class);
+      Long e = converter.convert(b, Long.class);
+      return (long) Math.pow(d, e);
+    }
+    if (aNum && bNum) {
+      Double d = converter.convert(a, Double.class);
+      Double e = converter.convert(b, Double.class);
+      return Math.pow(d, e);
+    }
+    throw new IllegalArgumentException(String.format("Unsupported operand type(s) for **: '%s' ('%s') and '%s' ('%s')",
+                                                     a,
+                                                     (a == null ? "null" : a.getClass().getSimpleName()),
+                                                     b,
+                                                     (b == null ? "null" : b.getClass().getSimpleName())
+    )
+    );
   }
 
   public static final Scanner.ExtensionToken TOKEN = new Scanner.ExtensionToken("**");
