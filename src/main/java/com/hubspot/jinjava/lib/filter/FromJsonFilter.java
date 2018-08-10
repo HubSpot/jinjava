d424919977bc5d4c0f755aa5bf4649d76dd24aa4
Matt Coley <mcoley@hubspot.com>
2018-08-10 12:10:53 -0400

    Add filter to convert JSON string to Map.

--- src/main/java/com/hubspot/jinjava/lib/filter/FromJsonFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/FromJsonFilter.java
@@ -1,6 +1,8 @@
 package com.hubspot.jinjava.lib.filter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+import java.util.HashMap;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
@@ -8,32 +10,37 @@ import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
 import com.hubspot.jinjava.interpret.InterpretException;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 
-
 @JinjavaDoc(
-    value = "Writes object as a JSON string",
+    value = "Converts JSON string to Object",
     params = {
-        @JinjavaParam(value = "o", desc = "Object to write to JSON")
+        @JinjavaParam(value = "s", desc = "JSON String to write to object")
     },
     snippets = {
         @JinjavaSnippet(
-            code = "{{object|tojson}}"
+            code = "{{object|fromJson}}"
         )
     })
-public class ToJsonFilter implements Filter {
+public class FromJsonFilter implements Filter {
 
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   @Override
   public Object filter(Object var, JinjavaInterpreter interpreter, String... args) {
     try {
-      return OBJECT_MAPPER.writeValueAsString(var);
-    } catch (JsonProcessingException e) {
-      throw new InterpretException("Could not write object as string for `tojson` filter.", e, interpreter.getLineNumber());
+
+      if (var instanceof String) {
+        return OBJECT_MAPPER.readValue((String) var, HashMap.class);
+      } else {
+        throw new InterpretException(String.format("%s filter requires a string parameter", getName()));
+      }
+
+    } catch (IOException e) {
+      throw new InterpretException("Could not convert JSON string to object in `fromjson` filter.", e, interpreter.getLineNumber());
     }
   }
 
   @Override
   public String getName() {
-    return "tojson";
+    return "fromjson";
   }
 }
