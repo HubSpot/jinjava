632dab2b46afbbefb9b58750d0b9ced651953ff7
Libo Song <lsong@hubspot.com>
2018-08-15 12:59:51 -0400

    Update.

--- src/main/java/com/hubspot/jinjava/lib/filter/SelectAttrFirstFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/SelectAttrFirstFilter.java
@@ -1,34 +1,34 @@
 
 package com.hubspot.jinjava.lib.filter;
 
-    import java.util.ArrayList;
     import java.util.Arrays;
-    import java.util.List;
-    import java.util.Map;
+import java.util.Map;
 
-    import com.google.common.collect.ImmutableMap;
-    import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
-    import com.hubspot.jinjava.doc.annotations.JinjavaParam;
-    import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
-    import com.hubspot.jinjava.interpret.InterpretException;
-    import com.hubspot.jinjava.interpret.JinjavaInterpreter;
-    import com.hubspot.jinjava.lib.exptest.ExpTest;
-    import com.hubspot.jinjava.util.ForLoop;
-    import com.hubspot.jinjava.util.ObjectIterator;
+import com.google.common.collect.ImmutableMap;
+import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
+import com.hubspot.jinjava.doc.annotations.JinjavaParam;
+import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
+import com.hubspot.jinjava.interpret.InterpretException;
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.lib.exptest.ExpTest;
+import com.hubspot.jinjava.util.ForLoop;
+import com.hubspot.jinjava.util.ObjectIterator;
 
 @JinjavaDoc(
-    value = "Filters a sequence of objects by applying a test to an attribute of an object and only selecting the ones with the test succeeding.",
+    value = "Filters a sequence of objects by applying a test to an" +
+        " attribute of an object and only selecting first with the" +
+        " test succeeding. This is similar to selectattr but is fast" +
+        " if you just want the first item",
     params = {
         @JinjavaParam(value = "sequence", type = "sequence", desc = "Sequence to test"),
         @JinjavaParam(value = "attr", desc = "Attribute to test for and select items that contain it"),
-        @JinjavaParam(value = "exp_test", type = "name of expression test", defaultValue = "truthy", desc = "Specify which expression test to run for making the selection")
+        @JinjavaParam(value = "exp_test", type = "name of expression test", defaultValue = "truthy",
+            desc = "Specify which expression test to run for making the selection")
     },
     snippets = {
         @JinjavaSnippet(
-            desc = "This loop would select any post containing content.post_list_summary_featured_image",
-            code = "{% for content in contents|selectattr('post_list_summary_featured_image') %}\n" +
-                "    <div class=\"post-item\">Post in listing markup</div>\n" +
-                "{% endfor %}")
+            desc = "This expression would select the first containing content.post_list_summary_featured_image",
+            code = "{{ contents|selectattrfirst('post_list_summary_featured_image') }}")
     })
 public class SelectAttrFirstFilter implements AdvancedFilter {
 
@@ -41,8 +41,6 @@ public class SelectAttrFirstFilter implements AdvancedFilter {
   public Object filter(Object var, JinjavaInterpreter interpreter, Object[] args, Map<String, Object> kwargs) {
     interpreter.startRender(getName());
     try {
-      List<Object> result = new ArrayList<>();
-
       if (args.length == 0) {
         throw new InterpretException(getName() + " filter requires an attr to filter on", interpreter.getLineNumber());
       }
@@ -79,15 +77,10 @@ public class SelectAttrFirstFilter implements AdvancedFilter {
         Object attrVal = interpreter.resolveProperty(val, attr);
 
         if (expTest.evaluate(attrVal, interpreter, expArgs)) {
-          result.add(val);
-          break;
+          return val;
         }
       }
-      if (result.size() > 0) {
-        return result.get(0);
-      } else {
-        return null;
-      }
+      return null;
     } finally {
       interpreter.endRender(getName(), ImmutableMap.of("attr", Arrays.toString(args), "kwargs", kwargs));
     }
---
+++ src/test/java/com/hubspot/jinjava/lib/filter/SelectAttrFirstFilterTest.java
@@ -0,0 +1,86 @@
+package com.hubspot.jinjava.lib.filter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.hubspot.jinjava.Jinjava;
+
+public class SelectAttrFirstFilterTest {
+
+  Jinjava jinjava;
+
+  @Before
+  public void setup() {
+    jinjava = new Jinjava();
+    jinjava.getGlobalContext().put("users", Lists.newArrayList(
+        new SelectAttrFirstFilterTest.User(0, false, "foo@bar.com"),
+        new SelectAttrFirstFilterTest.User(1, true, "bar@bar.com"),
+        new SelectAttrFirstFilterTest.User(2, false, null),
+        new SelectAttrFirstFilterTest.User(3, true, "bar@bar.com")));
+  }
+
+  @Test
+  public void selectAttrWithNoExp() {
+    assertThat(jinjava.render("{{ users|selectattrfirst('is_active') }}", new HashMap<String, Object>()))
+        .isEqualTo("1");
+  }
+
+  @Test
+  public void selectAttrWithExp() {
+    assertThat(jinjava.render("{{ users|selectattrfirst('email', 'none') }}", new HashMap<String, Object>()))
+        .isEqualTo("2");
+  }
+
+  @Test
+  public void selectAttrWithIsEqualToExp() {
+    assertThat(jinjava.render("{{ users|selectattrfirst('email', 'equalto', 'bar@bar.com') }}", new HashMap<String, Object>()))
+        .isEqualTo("1");
+  }
+
+  @Test
+  public void selectAttrWithNumericIsEqualToExp() {
+    assertThat(jinjava.render("{{ users|selectattrfirst('num', 'equalto', 1) }}", new HashMap<String, Object>()))
+        .isEqualTo("1");
+  }
+
+  @Test
+  public void selectAttrWithNoMatch() {
+    assertThat(jinjava.render("{{ users|selectattrfirst('email', 'equalto', 'abc') }}", new HashMap<String, Object>()))
+        .isEqualTo("");
+  }
+
+  public static class User {
+    private long num;
+    private boolean isActive;
+    private String email;
+
+    public User(long num, boolean isActive, String email) {
+      this.num = num;
+      this.isActive = isActive;
+      this.email = email;
+    }
+
+    public long getNum() {
+      return num;
+    }
+
+    public String getEmail() {
+      return email;
+    }
+
+    public boolean getIsActive() {
+      return isActive;
+    }
+
+    @Override
+    public String toString() {
+      return num + "";
+    }
+  }
+
+}
