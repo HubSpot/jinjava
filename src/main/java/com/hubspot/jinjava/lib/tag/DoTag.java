6fc2d3cb8cbffbf9f3123121ac5d1c345494bfe0
Matt Coley <mcoley@hubspot.com>
2018-10-11 11:55:35 -0400

    Implement 'do' tag

---
+++ src/main/java/com/hubspot/jinjava/lib/tag/DoTag.java
@@ -0,0 +1,40 @@
+package com.hubspot.jinjava.lib.tag;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
+import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.interpret.TemplateSyntaxException;
+import com.hubspot.jinjava.tree.TagNode;
+
+@JinjavaDoc(
+    value = "Evaluates expression without printing out result.",
+    snippets = {
+        @JinjavaSnippet(
+            code = "{% do list.append('value 2') %}")
+    })
+public class DoTag implements Tag {
+
+  @Override
+  public String interpret(TagNode tagNode, JinjavaInterpreter interpreter) {
+
+    if (StringUtils.isBlank(tagNode.getHelpers())) {
+      throw new TemplateSyntaxException(tagNode.getMaster().getImage(), "Tag 'do' expects expression", tagNode.getLineNumber(), tagNode.getStartPosition());
+    }
+
+    String expression = tagNode.getHelpers();
+    interpreter.resolveELExpression(expression, tagNode.getLineNumber());
+    return "";
+  }
+
+  @Override
+  public String getEndTagName() {
+    return null;
+  }
+
+  @Override
+  public String getName() {
+    return "do";
+  }
+}
