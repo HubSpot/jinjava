8823dd5e3a37bff2fabc5b515383dd38cd36545d
apizzurro <Gl9LmCJAiJgVV9MGkx1667p6dnqpO1LieoiKxM5B>
2018-11-15 16:16:49 -0500

    Catch PatternSyntaxException

--- src/main/java/com/hubspot/jinjava/lib/filter/RegexReplaceFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/RegexReplaceFilter.java
@@ -1,16 +1,19 @@
 package com.hubspot.jinjava.lib.filter;
 
+import com.google.re2j.Matcher;
+import com.google.re2j.Pattern;
+import com.google.re2j.PatternSyntaxException;
+
 import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
 import com.hubspot.jinjava.interpret.InterpretException;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
-import com.google.re2j.Matcher;
-import com.google.re2j.Pattern;
 
 @JinjavaDoc(
-        value = "Return a copy of the value with all occurrences of a matched regular expression replaced with a new one. " +
-                "The first argument is the regular expression to be matched, the second is the replacement string",
+        value = "Return a copy of the value with all occurrences of a matched regular expression (Java RE2 syntax) " +
+                "replaced with a new one. The first argument is the regular expression to be matched, the second " +
+                "is the replacement string",
         params = {
                 @JinjavaParam(value = "s", desc = "Base string to find and replace within"),
                 @JinjavaParam(value = "regex", desc = "The regular expression that you want to match and replace"),
@@ -38,15 +41,19 @@ public class RegexReplaceFilter implements Filter {
         if (args.length < 2) {
             throw new InterpretException("filter " + getName() + " requires two string args");
         }
-
         if (var instanceof String) {
+            String s = (String) var;
             String toReplace = args[0];
             String replaceWith = args[1];
-            String s = (String) var;
 
-            Pattern p = Pattern.compile(toReplace);
-            Matcher matcher = p.matcher(s);
-            return matcher.replaceAll(replaceWith);
+            try {
+                Pattern p = Pattern.compile(toReplace);
+                Matcher matcher = p.matcher(s);
+
+                return matcher.replaceAll(replaceWith);
+            } catch(PatternSyntaxException e) {
+                throw new InterpretException(getName() + " filter requires a valid regular expression");
+            }
         } else {
             throw new InterpretException(getName() + " filter requires a string parameter");
         }
