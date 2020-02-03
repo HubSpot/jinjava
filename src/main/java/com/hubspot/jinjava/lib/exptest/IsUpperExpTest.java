eecee50f87afd378c462e2efd4bc1d56f0b39181
Joe O'Hara <joe.is.ohara@gmail.com>
2020-02-03 17:18:15 +0000

    Handle SafeString in IsUpperExp

--- src/main/java/com/hubspot/jinjava/lib/exptest/IsUpperExpTest.java
+++ src/main/java/com/hubspot/jinjava/lib/exptest/IsUpperExpTest.java
@@ -6,6 +6,7 @@ import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.objects.SafeString;
 
 @JinjavaDoc(
     value = "Return true if string is all uppercased",
@@ -26,11 +27,11 @@ public class IsUpperExpTest implements ExpTest {
   @Override
   public boolean evaluate(Object var, JinjavaInterpreter interpreter,
       Object... args) {
-    if (!(var instanceof String)) {
+    if (!(var instanceof String || var instanceof SafeString)) {
       return false;
     }
 
-    return StringUtils.isAllUpperCase((String) var);
+    return StringUtils.isAllUpperCase(var.toString());
   }
 
 }
