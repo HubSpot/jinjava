77109640eebcc78d7d81c381c2bb92405d809a51
Jeff Boulter <jboulter@hubspot.com>
2020-07-15 17:40:37 -0400

    handle java exceptions with format filter

--- src/main/java/com/hubspot/jinjava/lib/filter/FormatFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/FormatFilter.java
@@ -3,8 +3,13 @@ package com.hubspot.jinjava.lib.filter;
 import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
+import com.hubspot.jinjava.interpret.InvalidArgumentException;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatException;
 import java.util.Map;
+import java.util.MissingFormatArgumentException;
+import java.util.MissingFormatWidthException;
 import java.util.Objects;
 
 @JinjavaDoc(
@@ -29,10 +34,11 @@ import java.util.Objects;
   }
 )
 public class FormatFilter implements AdvancedFilter {
+  public static final String NAME = "format";
 
   @Override
   public String getName() {
-    return "format";
+    return NAME;
   }
 
   @Override
@@ -42,7 +48,38 @@ public class FormatFilter implements AdvancedFilter {
     Object[] args,
     Map<String, Object> kwargs
   ) {
-    String fmt = Objects.toString(var, "");
-    return String.format(fmt, args);
+    try {
+      return String.format(Objects.toString(var, ""), args);
+    } catch (IllegalFormatException e) {
+      if (e instanceof MissingFormatArgumentException) {
+        throw new InvalidArgumentException(
+          interpreter,
+          NAME,
+          "Missing format argument for '" +
+          ((MissingFormatArgumentException) e).getFormatSpecifier() +
+          "'"
+        );
+      } else if (e instanceof IllegalFormatConversionException) {
+        throw new InvalidArgumentException(
+          interpreter,
+          NAME,
+          "'" +
+          args[0] +
+          "' is not a compatible type for conversion to format specifier '" +
+          ((IllegalFormatConversionException) e).getConversion() +
+          "'"
+        );
+      } else if (e instanceof MissingFormatWidthException) {
+        throw new InvalidArgumentException(
+          interpreter,
+          NAME,
+          "'" +
+          ((MissingFormatWidthException) e).getFormatSpecifier() +
+          "' is missing a width"
+        );
+      }
+      // TODO: possibly handle other subclasses of IllegalFormatException if they come up
+      throw new InvalidArgumentException(interpreter, NAME, e.getMessage());
+    }
   }
 }
