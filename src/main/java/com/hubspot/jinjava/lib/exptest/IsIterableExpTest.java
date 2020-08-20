3e8f3920130906d2084dd5239357e068dc0168ce
Matt Coley <matthewjcoley@gmail.com>
2020-08-20 14:43:35 -0400

    Revert "Updated pymap to behave as iterable without implementing it"

--- src/main/java/com/hubspot/jinjava/lib/exptest/IsIterableExpTest.java
+++ src/main/java/com/hubspot/jinjava/lib/exptest/IsIterableExpTest.java
@@ -4,8 +4,6 @@ import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
-import com.hubspot.jinjava.objects.SafeString;
-import com.hubspot.jinjava.objects.collections.PyMap;
 
 @JinjavaDoc(
   value = "Return true if the object is iterable (sequence, dict, etc)",
@@ -27,16 +25,9 @@ public class IsIterableExpTest implements ExpTest {
 
   @Override
   public boolean evaluate(Object var, JinjavaInterpreter interpreter, Object... args) {
-    return isIterable(var);
-  }
-
-  static boolean isIterable(Object ob) {
     return (
-      ob instanceof Object[] ||
-      ob instanceof Iterable ||
-      ob instanceof PyMap ||
-      ob instanceof String ||
-      ob instanceof SafeString
+      var != null &&
+      (var.getClass().isArray() || Iterable.class.isAssignableFrom(var.getClass()))
     );
   }
 }
--- src/main/java/com/hubspot/jinjava/lib/exptest/IsWithinExpTest.java
+++ src/main/java/com/hubspot/jinjava/lib/exptest/IsWithinExpTest.java
@@ -1,9 +1,31 @@
 package com.hubspot.jinjava.lib.exptest;
 
 import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
+import com.hubspot.jinjava.doc.annotations.JinjavaParam;
+import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 
-@JinjavaDoc(value = "", aliasOf = "in")
-public class IsWithinExpTest extends IsInExpTest {
+@JinjavaDoc(
+  value = "Returns true if a value is within a list",
+  input = @JinjavaParam(value = "value", type = "object", required = true),
+  params = @JinjavaParam(
+    value = "list",
+    type = "list",
+    desc = "A list to check if the value is in.",
+    required = true
+  ),
+  snippets = { @JinjavaSnippet(code = "{{ 2 is within [1, 2, 3] }}") }
+)
+public class IsWithinExpTest extends CollectionExpTest {
+
+  @Override
+  public boolean evaluate(Object var, JinjavaInterpreter interpreter, Object... args) {
+    if (args == null || args.length == 0) {
+      return false;
+    }
+
+    return (Boolean) COLLECTION_MEMBERSHIP_OPERATOR.apply(TYPE_CONVERTER, var, args[0]);
+  }
 
   @Override
   public String getName() {
