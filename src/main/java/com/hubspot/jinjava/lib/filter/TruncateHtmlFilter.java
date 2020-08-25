5ec3305175d4d6017562bca069f436a159e6039e
Liam Harwood <lharwood@hubspot.com>
2020-08-25 11:20:40 -0400

    Fix prettier

--- src/main/java/com/hubspot/jinjava/lib/filter/TruncateHtmlFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/TruncateHtmlFilter.java
@@ -37,13 +37,13 @@ import org.jsoup.select.NodeVisitor;
       type = "boolean",
       defaultValue = "false",
       desc = "If set to true, text will be truncated in the middle of words"
-    ),
+    )
   },
   snippets = {
     @JinjavaSnippet(
       code = "{{ \"<p>I want to truncate this text without breaking my HTML<p>\"|truncatehtml(28, '..', false) }}",
       output = "<p>I want to truncate this text without breaking my HTML</p>"
-    ),
+    )
   }
 )
 public class TruncateHtmlFilter implements Filter {
