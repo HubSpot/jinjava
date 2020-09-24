e1c034592760554b9aa060e0cba8abb38b50b6c8
Matt Coley <mcoley@hubspot.com>
2020-09-24 10:39:18 -0400

    Revert "Revert "Fix merge conflict'""
    This reverts commit 5b944cb5e6422e3256b63c5d91391edbd49a9766.

--- src/main/java/com/hubspot/jinjava/lib/exptest/IsFloatExpTest.java
+++ src/main/java/com/hubspot/jinjava/lib/exptest/IsFloatExpTest.java
@@ -4,28 +4,32 @@ import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
-import com.hubspot.jinjava.util.ObjectTruthValue;
+import java.math.BigDecimal;
 
 @JinjavaDoc(
-  value = "Return true if object is 'truthy'",
+  value = "Return true if object is a float",
   input = @JinjavaParam(value = "value", type = "object", required = true),
   snippets = {
     @JinjavaSnippet(
-      code = "{% if variable is truthy %}\n" +
-      "      <!--code to render a boolean variable is True-->\n" +
+      code = "{% if num is float %}\n" +
+      "      <!--code to render if num contains an floating point value-->\n" +
       "{% endif %}"
     )
   }
 )
-public class IsTruthyExpTest implements ExpTest {
+public class IsFloatExpTest implements ExpTest {
 
   @Override
   public String getName() {
-    return "truthy";
+    return "float";
   }
 
   @Override
   public boolean evaluate(Object var, JinjavaInterpreter interpreter, Object... args) {
-    return ObjectTruthValue.evaluate(var);
+    return (
+      var instanceof Double ||
+      var instanceof Float ||
+      (var instanceof BigDecimal && ((BigDecimal) var).scale() > 0)
+    );
   }
 }
