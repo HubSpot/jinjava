a571c8a631eee97026738842029e055ba61c448d
Michael Protopopov <michael.protopopov@marlo.com.au>
2020-11-12 22:22:58 +1100

    join, slice, sort, split, sum filter migrated to AbstractFilter. join filter: `attr` renamed to `attribute` matching unit test

--- src/main/java/com/hubspot/jinjava/lib/filter/AbstractFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/AbstractFilter.java
@@ -26,7 +26,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.commons.lang3.BooleanUtils;
