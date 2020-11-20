93316e7e68f0ca17551d55b799eb0b3385c48e0d
Jack Smith <jasmith@hubspot.com>
2020-11-20 09:19:40 -0500

    Allow aliased storage of MacroFunctions upon declaration

--- src/main/java/com/hubspot/jinjava/lib/tag/MacroTag.java
+++ src/main/java/com/hubspot/jinjava/lib/tag/MacroTag.java
@@ -6,13 +6,16 @@ import com.google.common.collect.Lists;
 import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
+import com.hubspot.jinjava.interpret.DeferredValue;
 import com.hubspot.jinjava.interpret.DeferredValueException;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.interpret.TemplateSyntaxException;
 import com.hubspot.jinjava.lib.fn.MacroFunction;
 import com.hubspot.jinjava.tree.TagNode;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
@@ -56,7 +59,11 @@ public class MacroTag implements Tag {
 
   private static final long serialVersionUID = 8397609322126956077L;
 
-  private static final Pattern MACRO_PATTERN = Pattern.compile(
+  public static final Pattern CHILD_MACRO_PATTERN = Pattern.compile(
+    "([a-zA-Z_][\\w_]*)\\.([a-zA-Z_][\\w_]*)[^(]*\\(([^)]*)\\)"
+  );
+
+  public static final Pattern MACRO_PATTERN = Pattern.compile(
     "([a-zA-Z_][\\w_]*)[^(]*\\(([^)]*)\\)"
   );
   private static final Splitter ARGS_SPLITTER = Splitter
@@ -75,22 +82,91 @@ public class MacroTag implements Tag {
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public String interpret(TagNode tagNode, JinjavaInterpreter interpreter) {
-    Matcher matcher = MACRO_PATTERN.matcher(tagNode.getHelpers());
-    if (!matcher.find()) {
-      throw new TemplateSyntaxException(
-        tagNode.getMaster().getImage(),
-        "Unable to parse macro definition: " + tagNode.getHelpers(),
+    String name;
+    String args;
+    String parentName = "";
+    Matcher childMatcher = CHILD_MACRO_PATTERN.matcher(tagNode.getHelpers());
+    if (childMatcher.find()) {
+      parentName = childMatcher.group(1);
+      name = childMatcher.group(2);
+      args = Strings.nullToEmpty(childMatcher.group(3));
+    } else {
+      Matcher matcher = MACRO_PATTERN.matcher(tagNode.getHelpers());
+      if (!matcher.find()) {
+        throw new TemplateSyntaxException(
+          tagNode.getMaster().getImage(),
+          "Unable to parse macro definition: " + tagNode.getHelpers(),
+          tagNode.getLineNumber(),
+          tagNode.getStartPosition()
+        );
+      }
+
+      name = matcher.group(1);
+      args = Strings.nullToEmpty(matcher.group(2));
+    }
+
+    LinkedHashMap<String, Object> argNamesWithDefaults = new LinkedHashMap<>();
+
+    boolean deferred = populateArgNames(
+      tagNode.getLineNumber(),
+      interpreter,
+      args,
+      argNamesWithDefaults
+    );
+
+    MacroFunction macro = new MacroFunction(
+      tagNode.getChildren(),
+      name,
+      argNamesWithDefaults,
+      false,
+      interpreter.getContext(),
+      interpreter.getLineNumber(),
+      interpreter.getPosition()
+    );
+    macro.setDeferred(deferred);
+
+    if (StringUtils.isNotEmpty(parentName)) {
+      try {
+        if (!(interpreter.getContext().get(parentName) instanceof DeferredValue)) {
+          Map<String, Object> macroOfParent = (Map<String, Object>) interpreter
+            .getContext()
+            .getOrDefault(parentName, new HashMap<>());
+          macroOfParent.put(macro.getName(), macro);
+          if (!interpreter.getContext().containsKey(parentName)) {
+            interpreter.getContext().put(parentName, macroOfParent);
+          }
+        }
+      } catch (ClassCastException e) {
+        throw new TemplateSyntaxException(
+          tagNode.getMaster().getImage(),
+          "Unable to parse macro as a child of: " + parentName,
+          tagNode.getLineNumber(),
+          tagNode.getStartPosition()
+        );
+      }
+    } else {
+      interpreter.getContext().addGlobalMacro(macro);
+    }
+
+    if (deferred) {
+      throw new DeferredValueException(
+        name,
         tagNode.getLineNumber(),
         tagNode.getStartPosition()
       );
     }
 
-    String name = matcher.group(1);
-    String args = Strings.nullToEmpty(matcher.group(2));
+    return "";
+  }
 
-    LinkedHashMap<String, Object> argNamesWithDefaults = new LinkedHashMap<>();
-
+  public static boolean populateArgNames(
+    int lineNumber,
+    JinjavaInterpreter interpreter,
+    String args,
+    LinkedHashMap<String, Object> argNamesWithDefaults
+  ) {
     List<String> argList = Lists.newArrayList(ARGS_SPLITTER.split(args));
     boolean deferred = false;
     for (int i = 0; i < argList.size(); i++) {
@@ -114,7 +190,7 @@ public class MacroTag implements Tag {
         try {
           Object argVal = interpreter.resolveELExpression(
             argValStr.toString(),
-            tagNode.getLineNumber()
+            lineNumber
           );
           argNamesWithDefaults.put(argName, argVal);
         } catch (DeferredValueException e) {
@@ -124,28 +200,6 @@ public class MacroTag implements Tag {
         argNamesWithDefaults.put(arg, null);
       }
     }
-
-    MacroFunction macro = new MacroFunction(
-      tagNode.getChildren(),
-      name,
-      argNamesWithDefaults,
-      false,
-      interpreter.getContext(),
-      interpreter.getLineNumber(),
-      interpreter.getPosition()
-    );
-    macro.setDeferred(deferred);
-
-    interpreter.getContext().addGlobalMacro(macro);
-
-    if (deferred) {
-      throw new DeferredValueException(
-        name,
-        tagNode.getLineNumber(),
-        tagNode.getStartPosition()
-      );
-    }
-
-    return "";
+    return deferred;
   }
 }
