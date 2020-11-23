cd0d53dbb1460ed856c49da5601e7b6d628a581e
Matt Coley <matthewjcoley@gmail.com>
2020-11-23 11:29:21 -0500

    Revert "Revert "Revert "Revert "Filter upgrades to support kw params""""

--- src/main/java/com/hubspot/jinjava/lib/filter/TruncateFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/TruncateFilter.java
@@ -20,6 +20,7 @@ import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.lib.fn.Functions;
+import java.util.Map;
 
 @JinjavaDoc(
   value = "Return a truncated copy of the string. The length is specified with the first parameter which defaults to 255. " +
@@ -33,19 +34,19 @@ import com.hubspot.jinjava.lib.fn.Functions;
   ),
   params = {
     @JinjavaParam(
-      value = "length",
-      type = "number",
+      value = TruncateFilter.LENGTH_PARAM,
+      type = "int",
       defaultValue = "255",
       desc = "Specifies the length at which to truncate the text (includes HTML characters)"
     ),
     @JinjavaParam(
-      value = "killwords",
+      value = TruncateFilter.KILLWORDS_PARAM,
       type = "boolean",
       defaultValue = "False",
       desc = "If true, the string will cut text at length"
     ),
     @JinjavaParam(
-      value = "end",
+      value = TruncateFilter.END_PARAM,
       defaultValue = "...",
       desc = "The characters that will be added to indicate where the text was truncated"
     )
@@ -61,11 +62,21 @@ import com.hubspot.jinjava.lib.fn.Functions;
     )
   }
 )
-public class TruncateFilter implements Filter {
+public class TruncateFilter extends AbstractFilter implements Filter {
+  public static final String LENGTH_PARAM = "length";
+  public static final String KILLWORDS_PARAM = "killwords";
+  public static final String END_PARAM = "end";
 
   @Override
-  public Object filter(Object object, JinjavaInterpreter interpreter, String... arg) {
-    return Functions.truncate(object, arg);
+  public Object filter(
+    Object object,
+    JinjavaInterpreter interpreter,
+    Map<String, Object> parsedArgs
+  ) {
+    int length = (int) parsedArgs.get(LENGTH_PARAM);
+    boolean killwords = (boolean) parsedArgs.get(KILLWORDS_PARAM);
+    String end = (String) parsedArgs.get(END_PARAM);
+    return Functions.truncate(object, length, killwords, end);
   }
 
   @Override
