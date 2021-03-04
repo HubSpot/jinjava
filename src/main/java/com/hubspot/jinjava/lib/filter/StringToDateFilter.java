66a74a8f1b4ed76ef8620be071ff061fe540f8a2
Liam Harwood <lharwood@hubspot.com>
2021-03-04 11:33:07 -0500

    Add jinjava doc

--- src/main/java/com/hubspot/jinjava/lib/filter/StringToDateFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/StringToDateFilter.java
@@ -1,11 +1,26 @@
 package com.hubspot.jinjava.lib.filter;
 
+import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
+import com.hubspot.jinjava.doc.annotations.JinjavaParam;
+import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
 import com.hubspot.jinjava.interpret.InvalidInputException;
 import com.hubspot.jinjava.interpret.InvalidReason;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.interpret.TemplateSyntaxException;
 import com.hubspot.jinjava.lib.fn.Functions;
 
+@JinjavaDoc(
+  value = "Converts a date string and date format to a date object",
+  input = @JinjavaParam(value = "dateString", desc = "Date string", required = true),
+  params = {
+    @JinjavaParam(
+      value = "dateFormat",
+      desc = "Format of the date string",
+      required = true
+    ),
+  },
+  snippets = { @JinjavaSnippet(code = "{{ '3/3/21'|strtodate('M/d/yy') }}") }
+)
 public class StringToDateFilter implements Filter {
 
   @Override
