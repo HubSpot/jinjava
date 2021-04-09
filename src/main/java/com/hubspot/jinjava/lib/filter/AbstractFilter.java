c6cb75b03aed089b0a88703cdf4922472f060aac
Matt Coley <mcoley@hubspot.com>
2021-04-09 13:47:45 -0400

    fix bad FB

--- src/main/java/com/hubspot/jinjava/lib/filter/AbstractFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/AbstractFilter.java
@@ -21,6 +21,7 @@ import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.interpret.InvalidInputException;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -65,6 +66,9 @@ public abstract class AbstractFilter implements Filter {
     return filter(var, interpreter, args, Collections.emptyMap());
   }
 
+  @SuppressFBWarnings(
+    "FB bug prevents forEach() method call counting `namedArgs` as used (fixed in next release)"
+  )
   public Object filter(
     Object var,
     JinjavaInterpreter interpreter,
