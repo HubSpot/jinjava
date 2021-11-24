be95c30b292b572864946e799ad87d1ff3f65406
Jack Smith <jasmith@hubspot.com>
2021-11-24 13:08:42 -0500

    Avoid bug check while still checking if map or list contains itself

--- src/main/java/com/hubspot/jinjava/util/EagerReconstructionUtils.java
+++ src/main/java/com/hubspot/jinjava/util/EagerReconstructionUtils.java
@@ -201,8 +201,8 @@ public class EagerReconstructionUtils {
   private static Object getObjectOrHashCode(Object o) {
     if (o instanceof PyList || o instanceof PyMap) {
       if (
-        (o instanceof PyList && ((PyList) o).contains(o)) ||
-        (o instanceof PyMap && ((PyMap) o).containsValue(o))
+        (o instanceof PyList && ((PyList) o).toList().contains(o)) ||
+        (o instanceof PyMap && ((PyMap) o).toMap().containsValue(o))
       ) {
         return o; // Can't run hashcode as it will have infinite recursion and cause a stack overflow
       }
