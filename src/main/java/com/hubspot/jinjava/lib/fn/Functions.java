23e2b99b2887a991ce9ce62903de96f6e5ec2c96
James Roeder <jroeder@hubspot.com>
2022-02-24 18:13:06 -0500

    Revert to default timezone if unspecified

--- src/main/java/com/hubspot/jinjava/lib/fn/Functions.java
+++ src/main/java/com/hubspot/jinjava/lib/fn/Functions.java
@@ -161,11 +161,11 @@ public class Functions {
   public static String dateTimeFormat(Object var, String... format) {
     ZoneId zoneOffset = ZoneOffset.UTC;
 
-    if (format.length > 1) {
+    if (format.length > 1 && format[1] != null) {
       String timezone = format[1];
       try {
         zoneOffset = ZoneId.of(timezone);
-      } catch (DateTimeException | NullPointerException e) {
+      } catch (DateTimeException e) {
         throw new InvalidArgumentException(
           JinjavaInterpreter.getCurrent(),
           "datetimeformat",
