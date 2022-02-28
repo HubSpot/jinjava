07e236c54cac2ee52028ab476c370e1399442f16
Jack Smith <jasmith@hubspot.com>
2022-02-28 14:11:49 -0500

    Handle serialization of null keys as empty string in maps with pyish serialization

---
+++ src/test/java/com/hubspot/jinjava/objects/serialization/PyishObjectMapperTest.java
@@ -0,0 +1,20 @@
+package com.hubspot.jinjava.objects.serialization;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hubspot.jinjava.objects.collections.SizeLimitingPyMap;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+public class PyishObjectMapperTest {
+
+  @Test
+  public void itSerializesMapWithNullKeysAsEmptyString() {
+    Map<String, Object> map = new SizeLimitingPyMap(new HashMap<>(), 10);
+    map.put("foo", "bar");
+    map.put(null, "null");
+    assertThat(PyishObjectMapper.getAsPyishString(map))
+      .isEqualTo("{'': 'null', 'foo': 'bar'}");
+  }
+}
