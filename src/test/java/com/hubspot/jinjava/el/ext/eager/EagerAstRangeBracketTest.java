274d6c87ed3b453393b823db0d2c1d771de8bb20
Jack Smith <jasmith@hubspot.com>
2022-03-18 16:35:04 -0400

    Handle null values in EvalResultHolder as empty strings

---
+++ src/test/java/com/hubspot/jinjava/el/ext/eager/EagerAstRangeBracketTest.java
@@ -0,0 +1,44 @@
+package com.hubspot.jinjava.el.ext.eager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hubspot.jinjava.BaseInterpretingTest;
+import com.hubspot.jinjava.JinjavaConfig;
+import com.hubspot.jinjava.LegacyOverrides;
+import com.hubspot.jinjava.interpret.Context;
+import com.hubspot.jinjava.interpret.DeferredValue;
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.mode.EagerExecutionMode;
+import com.hubspot.jinjava.random.RandomNumberGeneratorStrategy;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EagerAstRangeBracketTest extends BaseInterpretingTest {
+  @Before
+  public void setup() {
+    JinjavaConfig config = JinjavaConfig
+      .newBuilder()
+      .withRandomNumberGeneratorStrategy(RandomNumberGeneratorStrategy.DEFERRED)
+      .withExecutionMode(EagerExecutionMode.instance())
+      .withNestedInterpretationEnabled(true)
+      .withLegacyOverrides(
+        LegacyOverrides.newBuilder().withUsePyishObjectMapper(true).build()
+      )
+      .withMaxMacroRecursionDepth(5)
+      .withEnableRecursiveMacroCalls(true)
+      .build();
+    JinjavaInterpreter parentInterpreter = new JinjavaInterpreter(
+      jinjava,
+      new Context(),
+      config
+    );
+    interpreter = new JinjavaInterpreter(parentInterpreter);
+
+    interpreter.getContext().put("deferred", DeferredValue.instance());
+  }
+
+  @Test
+  public void itHandlesNullRangePrefix() {
+    assertThat(interpreter.render("{{ deferred[:100] }}")).isEqualTo("{{ deferred[:100] }}");
+  }
+}
