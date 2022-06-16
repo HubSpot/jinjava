e1713550af381071069bac2b606bffb91dd7d785
Jeff Boulter <jeff@boulter.com>
2022-06-16 17:38:51 -0400

    Revert "sort json attributes and maps"

--- src/test/java/com/hubspot/jinjava/lib/filter/ToJsonFilterTest.java
+++ src/test/java/com/hubspot/jinjava/lib/filter/ToJsonFilterTest.java
@@ -22,12 +22,9 @@ public class ToJsonFilterTest extends BaseInterpretingTest {
     assertThat(filter.filter(testArray, interpreter)).isEqualTo("[4,1,2]");
 
     Map<String, Object> testMap = new LinkedHashMap<>();
-    testMap.put("testString", "testString");
-    testMap.put("testBoolean", true);
     testMap.put("testArray", testArray);
+    testMap.put("testString", "testString");
     assertThat(filter.filter(testMap, interpreter))
-      .isEqualTo(
-        "{\"testArray\":[4,1,2],\"testBoolean\":true,\"testString\":\"testString\"}"
-      );
+      .isEqualTo("{\"testArray\":[4,1,2],\"testString\":\"testString\"}");
   }
 }
