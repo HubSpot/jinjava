2bbf47463a82bb02bc92f9eef02366a6e0238944
Jeff Boulter <jboulter@hubspot.com>
2022-06-16 15:55:28 -0400

    sort json attributes and maps

--- src/main/java/com/hubspot/jinjava/JinjavaConfig.java
+++ src/main/java/com/hubspot/jinjava/JinjavaConfig.java
@@ -17,7 +17,9 @@ package com.hubspot.jinjava;
 
 import static com.hubspot.jinjava.lib.fn.Functions.DEFAULT_RANGE_LIMIT;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.hubspot.jinjava.el.JinjavaInterpreterResolver;
 import com.hubspot.jinjava.interpret.Context;
 import com.hubspot.jinjava.interpret.Context.Library;
@@ -272,7 +274,14 @@ public class JinjavaConfig {
     private boolean enablePreciseDivideFilter = false;
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    private Builder() {}
+    private Builder() {
+      objectMapper.setConfig(
+        objectMapper
+          .getSerializationConfig()
+          .with(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+          .with(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
+      );
+    }
 
     public Builder withCharset(Charset charset) {
       this.charset = charset;
