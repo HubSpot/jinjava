e8300538a346edcce8a03b063de59b99b2025c46
Tyler Kindy <tkindy@hubspot.com>
2022-09-27 16:26:36 -0400

    Improve exception tests for stringToTime

--- src/test/java/com/hubspot/jinjava/lib/fn/StringToTimeFunctionTest.java
+++ src/test/java/com/hubspot/jinjava/lib/fn/StringToTimeFunctionTest.java
@@ -1,6 +1,7 @@
 package com.hubspot.jinjava.lib.fn;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import com.hubspot.jinjava.interpret.InterpretException;
 import com.hubspot.jinjava.objects.date.PyishDate;
@@ -20,24 +21,24 @@ public class StringToTimeFunctionTest {
     assertThat(Functions.stringToTime(datetime, format)).isEqualTo(expected);
   }
 
-  @Test(expected = InterpretException.class)
+  @Test
   public void itFailsOnInvalidFormat() {
     String datetime = "2018-07-14T14:31:30+0530";
     String format = "not a time format";
-    PyishDate expected = new PyishDate(
-      ZonedDateTime.of(2018, 7, 14, 14, 31, 30, 0, ZoneOffset.ofHoursMinutes(5, 30))
-    );
-    assertThat(Functions.stringToTime(datetime, format)).isEqualTo(expected);
+
+    assertThatExceptionOfType(InterpretException.class)
+      .isThrownBy(() -> Functions.stringToTime(datetime, format))
+      .withMessageContaining("requires valid datetime format");
   }
 
-  @Test(expected = InterpretException.class)
+  @Test
   public void itFailsOnTimeFormatMismatch() {
     String datetime = "Saturday, Jul 14, 2018 14:31:06 PM";
     String format = "yyyy-MM-dd'T'HH:mm:ssZ";
-    PyishDate expected = new PyishDate(
-      ZonedDateTime.of(2018, 7, 14, 14, 31, 30, 0, ZoneOffset.ofHoursMinutes(5, 30))
-    );
-    assertThat(Functions.stringToTime(datetime, format)).isEqualTo(expected);
+
+    assertThatExceptionOfType(InterpretException.class)
+      .isThrownBy(() -> Functions.stringToTime(datetime, format))
+      .withMessageContaining("could not match datetime input");
   }
 
   public void itReturnsNullOnNullInput() {
@@ -46,10 +47,13 @@ public class StringToTimeFunctionTest {
     assertThat(Functions.stringToTime(datetime, format)).isEqualTo(null);
   }
 
-  @Test(expected = InterpretException.class)
+  @Test
   public void itFailsOnNullDatetimeFormat() {
     String datetime = "2018-07-14T14:31:30+0530";
     String format = null;
-    assertThat(Functions.stringToTime(datetime, format)).isEqualTo(null);
+
+    assertThatExceptionOfType(InterpretException.class)
+      .isThrownBy(() -> Functions.stringToTime(datetime, format))
+      .withMessageContaining("requires non-null datetime format");
   }
 }
