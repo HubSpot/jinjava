dbf14854165cf294c5bdadd954cf2c1ed757e2e0
Tyler Kindy <tkindy@hubspot.com>
2022-09-28 15:19:44 -0400

    Add simple mapping conversion component

---
+++ src/main/java/com/hubspot/jinjava/objects/date/MappingStrftimeConversionComponent.java
@@ -0,0 +1,23 @@
+package com.hubspot.jinjava.objects.date;
+
+import java.time.format.DateTimeFormatterBuilder;
+
+public class MappingStrftimeConversionComponent implements StrftimeConversionComponent {
+  private final char sourcePattern;
+  private final String targetPattern;
+
+  public MappingStrftimeConversionComponent(char sourcePattern, String targetPattern) {
+    this.sourcePattern = sourcePattern;
+    this.targetPattern = targetPattern;
+  }
+
+  @Override
+  public char getSourcePattern() {
+    return sourcePattern;
+  }
+
+  @Override
+  public DateTimeFormatterBuilder append(DateTimeFormatterBuilder builder) {
+    return builder.appendPattern(targetPattern);
+  }
+}
