f32e0a6359298b7a9ed19ca3cf623a970af5dceb
Nathaniel Pautzke <npautzke@hubspot.com>
2022-10-20 11:47:07 -0500

    add unit test for eager cycle tag

--- src/test/java/com/hubspot/jinjava/lib/tag/eager/EagerCycleTagTest.java
+++ src/test/java/com/hubspot/jinjava/lib/tag/eager/EagerCycleTagTest.java
@@ -1,13 +1,17 @@
 package com.hubspot.jinjava.lib.tag.eager;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 import com.hubspot.jinjava.JinjavaConfig;
 import com.hubspot.jinjava.LegacyOverrides;
+import com.hubspot.jinjava.interpret.DeferredValue;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.lib.tag.CycleTagTest;
 import com.hubspot.jinjava.lib.tag.Tag;
 import com.hubspot.jinjava.mode.EagerExecutionMode;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class EagerCycleTagTest extends CycleTagTest {
   private static final long MAX_OUTPUT_SIZE = 500L;
@@ -31,6 +35,7 @@ public class EagerCycleTagTest extends CycleTagTest {
 
     tag = new EagerCycleTag();
     context.registerTag(tag);
+    context.put("deferred", DeferredValue.instance());
     context.registerTag(new EagerForTag());
     JinjavaInterpreter.pushCurrent(interpreter);
   }
@@ -38,5 +43,11 @@ public class EagerCycleTagTest extends CycleTagTest {
   @After
   public void teardown() {
     JinjavaInterpreter.popCurrent();
+  }
+
+  @Test
+  public void itHandlesDeferredCycle() {
+    String template = "{% for item in deferred %}{% cycle {{foo}},{{bar}} %}{% endfor %}";
+    assertThat(interpreter.render(template)).isEqualTo(template);
   }
 }
