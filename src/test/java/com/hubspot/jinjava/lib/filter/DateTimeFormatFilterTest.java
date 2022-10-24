82d83aa8efbea1d7fc67b6422426c844be0dd89f
Tyler Kindy <tkindy@hubspot.com>
2022-10-24 14:13:20 -0400

    Remove outdated assertion

--- src/test/java/com/hubspot/jinjava/lib/filter/DateTimeFormatFilterTest.java
+++ src/test/java/com/hubspot/jinjava/lib/filter/DateTimeFormatFilterTest.java
@@ -154,12 +154,5 @@ public class DateTimeFormatFilterTest extends BaseInterpretingTest {
     TemplateError error = result.getErrors().get(0);
     assertThat(error.getSeverity()).isEqualTo(ErrorType.FATAL);
     assertThat(error.getMessage()).contains("Invalid date format: [%é]");
-
-    /*
-    datetimeformat outputs the string "null" for unrecognized format codes,
-    which DateTimeFormatter then tries to interpret as a pattern. 'n' and 'u'
-    are valid pattern letters, but 'l' is not, hence the following error message.
-    */
-    assertThat(error.getMessage()).contains("Unknown pattern letter: l");
   }
 }
