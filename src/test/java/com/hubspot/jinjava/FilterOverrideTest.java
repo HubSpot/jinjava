18786a1b4118a20e56282d8973da1aefa92f7997
Tyler Kindy <tkindy@hubspot.com>
2022-11-10 16:55:21 -0500

    Add test for overriding built-in filters

---
+++ src/test/java/com/hubspot/jinjava/FilterOverrideTest.java
@@ -0,0 +1,40 @@
+package com.hubspot.jinjava;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.lib.filter.Filter;
+import java.util.HashMap;
+import org.junit.Test;
+
+public class FilterOverrideTest {
+
+  @Test
+  public void itAllowsUsersToOverrideBuiltInFilters() {
+    Jinjava jinjava = new Jinjava();
+    assertThat(jinjava.render("{{ 5 | add(6) }}", new HashMap<>())).isEqualTo("11");
+
+    jinjava.getGlobalContext().registerClasses(DescriptiveAddFilter.class);
+    assertThat(jinjava.render("{{ 5 | add(6) }}", new HashMap<>()))
+      .isEqualTo("5 + 6 = 11");
+  }
+
+  public static class DescriptiveAddFilter implements Filter {
+
+    @Override
+    public String getName() {
+      return "add";
+    }
+
+    @Override
+    public Object filter(Object var, JinjavaInterpreter interpreter, String... args) {
+      return (
+        var +
+        " + " +
+        args[0] +
+        " = " +
+        (Integer.parseInt(var.toString()) + Integer.parseInt(args[0]))
+      );
+    }
+  }
+}
