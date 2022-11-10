caf9cb4f6b25d3a054695d0fd4f65924745bf76a
Tyler Kindy <tkindy@hubspot.com>
2022-11-10 17:38:41 -0500

    Add skeleton datetime format filter

---
+++ src/main/java/com/hubspot/jinjava/lib/filter/FormatDatetimeFilter.java
@@ -0,0 +1,16 @@
+package com.hubspot.jinjava.lib.filter;
+
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+
+public class FormatDatetimeFilter implements Filter {
+
+  @Override
+  public Object filter(Object var, JinjavaInterpreter interpreter, String... args) {
+    return null;
+  }
+
+  @Override
+  public String getName() {
+    return null;
+  }
+}
