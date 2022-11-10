87b043ff1aac2c894750cfe445ffa0f3e3853c69
Tyler Kindy <tkindy@hubspot.com>
2022-11-10 18:20:08 -0500

    Remove LocalDateTime test
    It turns out datetimeformat doesn't support LocalDateTime, so I won't
    add it for the new filters.

--- src/test/java/com/hubspot/jinjava/lib/filter/FormatDatetimeFilterTest.java
+++ src/test/java/com/hubspot/jinjava/lib/filter/FormatDatetimeFilterTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.ImmutableMap;
 import com.hubspot.jinjava.Jinjava;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import org.junit.Before;
@@ -17,16 +16,6 @@ public class FormatDatetimeFilterTest {
   public void setUp() throws Exception {
     jinjava = new Jinjava();
     jinjava.getGlobalContext().registerClasses(FormatDatetimeFilter.class);
-  }
-
-  @Test
-  public void itFormatsLocalDateTimes() {
-    LocalDateTime dateTime = LocalDateTime.of(2022, 11, 10, 17, 49, 7);
-
-    assertThat(
-        jinjava.render("{{ d | format_datetime }}", ImmutableMap.of("d", dateTime))
-      )
-      .isEqualTo("Nov 10, 2022, 5:49:07 PM");
   }
 
   @Test
