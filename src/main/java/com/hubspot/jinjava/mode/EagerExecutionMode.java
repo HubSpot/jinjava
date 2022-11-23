ad26e1e694cebe992e7ee5b3f2bfe56319494abe
Jack Smith <jasmith@hubspot.com>
2022-11-23 12:39:49 -0500

    Add `current_path` to meta context variables

--- src/main/java/com/hubspot/jinjava/mode/EagerExecutionMode.java
+++ src/main/java/com/hubspot/jinjava/mode/EagerExecutionMode.java
@@ -5,10 +5,20 @@ import com.hubspot.jinjava.interpret.Context;
 import com.hubspot.jinjava.lib.expression.EagerExpressionStrategy;
 import com.hubspot.jinjava.lib.tag.eager.EagerTagDecorator;
 import com.hubspot.jinjava.lib.tag.eager.EagerTagFactory;
+import com.hubspot.jinjava.loader.RelativePathResolver;
 import java.util.Optional;
 
 public class EagerExecutionMode implements ExecutionMode {
   private static final ExecutionMode INSTANCE = new EagerExecutionMode();
+
+  // These meta context variables should never be removed from the set of meta context variables
+  public static final ImmutableSet<String> STATIC_META_CONTEXT_VARIABLES = ImmutableSet.of(
+    Context.GLOBAL_MACROS_SCOPE_KEY,
+    Context.IMPORT_RESOURCE_PATH_KEY,
+    Context.DEFERRED_IMPORT_RESOURCE_PATH_KEY,
+    Context.IMPORT_RESOURCE_ALIAS_KEY,
+    RelativePathResolver.CURRENT_PATH_CONTEXT_KEY
+  );
 
   protected EagerExecutionMode() {}
 
@@ -41,15 +51,6 @@ public class EagerExecutionMode implements ExecutionMode {
       .filter(Optional::isPresent)
       .forEach(maybeEagerTag -> context.registerTag(maybeEagerTag.get()));
     context.setExpressionStrategy(new EagerExpressionStrategy());
-    context
-      .getMetaContextVariables()
-      .addAll(
-        ImmutableSet.of(
-          Context.GLOBAL_MACROS_SCOPE_KEY,
-          Context.IMPORT_RESOURCE_PATH_KEY,
-          Context.DEFERRED_IMPORT_RESOURCE_PATH_KEY,
-          Context.IMPORT_RESOURCE_ALIAS_KEY
-        )
-      );
+    context.getMetaContextVariables().addAll(STATIC_META_CONTEXT_VARIABLES);
   }
 }
