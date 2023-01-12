e9a4b5c4a81ff34a31f8a0517fc347a7edd43180
Jack Smith <jasmith@hubspot.com>
2023-01-12 16:33:22 -0500

    Add test that filters and exp tests aren't needlessly preserved

--- src/test/java/com/hubspot/jinjava/EagerTest.java
+++ src/test/java/com/hubspot/jinjava/EagerTest.java
@@ -1271,4 +1271,11 @@ public class EagerTest {
       "correctly-preserves-identifiers-in-macro-function"
     );
   }
+
+  @Test
+  public void itDoesNotPreserveFilterAndExpTestIdentifiers() {
+    expectedTemplateInterpreter.assertExpectedOutputNonIdempotent(
+      "does-not-preserve-filter-and-exp-test-identifiers"
+    );
+  }
 }
