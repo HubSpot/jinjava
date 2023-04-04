bc31703180d727569bab8df36c6f05e4edebfddb
Jeff Boulter <jboulter@hubspot.com>
2023-04-04 18:06:47 -0400

    restore rendertimings

---
+++ src/main/java/com/hubspot/jinjava/interpret/RenderTimings.java
@@ -0,0 +1,10 @@
+package com.hubspot.jinjava.interpret;
+
+import java.util.Map;
+
+@Deprecated
+public interface RenderTimings {
+  void start(JinjavaInterpreter interpreter, String name);
+  void end(JinjavaInterpreter interpreter, String name);
+  void end(JinjavaInterpreter interpreter, String name, Map<String, Object> data);
+}
