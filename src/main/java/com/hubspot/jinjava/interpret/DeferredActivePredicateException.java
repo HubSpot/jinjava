01824534037bde85bf420b2eb340d4f1e2b07b25
Jack Smith <72623970+jasmith-hs@users.noreply.github.com>
2023-08-04 10:08:18 -0400

    Revert "add DeferredActivePredicateException"

--- src/main/java/com/hubspot/jinjava/interpret/DeferredActivePredicateException.java
+++
@@ -1,8 +0,0 @@
-package com.hubspot.jinjava.interpret;
-
-public class DeferredActivePredicateException extends DeferredValueException {
-
-  public DeferredActivePredicateException(String message) {
-    super(message);
-  }
-}
