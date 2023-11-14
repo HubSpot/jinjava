a6de2e21bb55b9de0eabfcd062d1fc0ff2d752c4
Jack Smith <jasmith@hubspot.com>
2023-11-14 16:19:50 -0500

    Add length-limiting to ToJsonFilter to avoid serializing a value which ends up larger than the max output size. This helps to prevent memory issues that could result from recursive referencing within an object

--- src/test/java/com/hubspot/jinjava/lib/filter/ToJsonFilterTest.java
+++ src/test/java/com/hubspot/jinjava/lib/filter/ToJsonFilterTest.java
@@ -3,7 +3,12 @@ package com.hubspot.jinjava.lib.filter;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 import com.hubspot.jinjava.BaseInterpretingTest;
+import com.hubspot.jinjava.Jinjava;
+import com.hubspot.jinjava.JinjavaConfig;
+import com.hubspot.jinjava.interpret.OutputTooBigException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,5 +31,30 @@ public class ToJsonFilterTest extends BaseInterpretingTest {
     testMap.put("testString", "testString");
     assertThat(filter.filter(testMap, interpreter))
       .isEqualTo("{\"testArray\":[4,1,2],\"testString\":\"testString\"}");
+  }
+
+  @Test
+  public void itLimitsLength() {
+    List<List<?>> original = new ArrayList<>();
+    List<List<?>> temp = original;
+    for (int i = 0; i < 100; i++) {
+      List<List<?>> nested = new ArrayList<>();
+      temp.add(nested);
+      temp = nested;
+    }
+    interpreter =
+      new Jinjava(JinjavaConfig.newBuilder().withMaxOutputSize(500).build())
+      .newInterpreter();
+    assertThat(filter.filter(original, interpreter)).asString().contains("[[]]]]");
+    for (int i = 0; i < 400; i++) {
+      List<List<?>> nested = new ArrayList<>();
+      temp.add(nested);
+      temp = nested;
+    }
+    try {
+      filter.filter(original, interpreter);
+    } catch (Exception e) {
+      assertThat(e).isInstanceOf(OutputTooBigException.class);
+    }
   }
 }
