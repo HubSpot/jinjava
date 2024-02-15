34e06d08800038bb463161b095f509762d7886cd
Jack Smith <jasmith@hubspot.com>
2024-02-15 15:35:06 -0500

    Add suite of tests for PartiallyDeferredValue functionality

--- src/test/java/com/hubspot/jinjava/interpret/PartiallyDeferredValueTest.java
+++ src/test/java/com/hubspot/jinjava/interpret/PartiallyDeferredValueTest.java
@@ -2,6 +2,7 @@ package com.hubspot.jinjava.interpret;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.ImmutableMap;
 import com.hubspot.jinjava.BaseInterpretingTest;
 import com.hubspot.jinjava.JinjavaConfig;
 import com.hubspot.jinjava.LegacyOverrides;
@@ -12,6 +13,7 @@ import com.hubspot.jinjava.random.RandomNumberGeneratorStrategy;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
+import javax.annotation.CheckForNull;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -50,6 +52,101 @@ public class PartiallyDeferredValueTest extends BaseInterpretingTest {
     assertThat(interpreter.getContext().getDeferredNodes()).isNotEmpty();
   }
 
+  @Test
+  public void itDefersNodeWhenCannotCallPartiallyDeferredMapEntrySet() {
+    interpreter
+      .getContext()
+      .put("foo", new BadEntrySet(ImmutableMap.of("resolved", "resolved")));
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.resolved }}"))
+      .isEqualTo("resolved");
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.deferred }}"))
+      .isEqualTo("{{ bar.deferred }}");
+    assertThat(interpreter.getContext().getDeferredNodes()).isNotEmpty();
+  }
+
+  @Test
+  public void itDefersNodeWhenPyishSerializationFails() {
+    interpreter.getContext().put("foo", new BadPyishSerializable());
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.resolved }}"))
+      .isEqualTo("resolved");
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.deferred }}"))
+      .isEqualTo("{{ bar.deferred }}");
+    assertThat(interpreter.getContext().getDeferredNodes()).isNotEmpty();
+  }
+
+  @Test
+  public void itSerializesWhenPyishSerializationIsGood() {
+    interpreter.getContext().put("foo", new GoodPyishSerializable());
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.resolved }}"))
+      .isEqualTo("resolved");
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.deferred }}"))
+      .isEqualTo("{{ good.deferred }}");
+    assertThat(interpreter.getContext().getDeferredNodes()).isEmpty();
+  }
+
+  @Test
+  public void itSerializesWhenEntrySetIsBadButItIsPyishSerializable() {
+    interpreter
+      .getContext()
+      .put(
+        "foo",
+        new BadEntrySetButPyishSerializable(ImmutableMap.of("resolved", "resolved"))
+      );
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.resolved }}"))
+      .isEqualTo("resolved");
+    assertThat(interpreter.render("{% set bar = foo %}{{ bar.deferred }}"))
+      .isEqualTo("{{ hello.deferred }}");
+    assertThat(interpreter.getContext().getDeferredNodes()).isEmpty();
+  }
+
+  @Test
+  public void itSerializesPartiallyDeferredValueIsInsideAMap() {
+    interpreter
+      .getContext()
+      .put("foo_map", new PyMap(ImmutableMap.of("foo", new GoodPyishSerializable())));
+    assertThat(interpreter.render("{% set bar = foo_map %}{{ bar.foo.resolved }}"))
+      .isEqualTo("resolved");
+    assertThat(interpreter.render("{% set bar = foo_map.foo %}{{ bar.resolved }}"))
+      .isEqualTo("resolved");
+    assertThat(interpreter.render("{% set bar = foo_map %}{{ bar.foo.deferred }}"))
+      .isEqualTo("{{ good.deferred }}");
+    assertThat(interpreter.render("{% set bar = foo_map.foo %}{{ bar.deferred }}"))
+      .isEqualTo("{{ good.deferred }}");
+    assertThat(interpreter.getContext().getDeferredNodes()).isEmpty();
+  }
+
+  @Test
+  public void itSerializesPartiallyDeferredValueIsPutInsideAMap() {
+    interpreter.getContext().put("foo", new GoodPyishSerializable());
+    assertThat(
+      interpreter.render("{% set bar = {'my_key': foo} %}{% print bar.my_key.resolved %}")
+    )
+      .isEqualTo("resolved");
+    assertThat(
+      interpreter.render("{% set bar = {'my_key': foo} %}{% print bar.my_key.deferred %}")
+    )
+      .isEqualTo("{% print good.deferred %}");
+    assertThat(interpreter.getContext().getDeferredNodes()).isEmpty();
+  }
+
+  @Test
+  public void itSerializesPartiallyDeferredValueIsPutInsideAMapInComplexExpression() {
+    interpreter.getContext().put("foo", new GoodPyishSerializable());
+    assertThat(
+      interpreter.render(
+        "{% set bar = {'my_key': foo} %}{% print (1 + 1 == 3 || bar.my_key.resolved) ~ '.' %}"
+      )
+    )
+      .isEqualTo("resolved.");
+    assertThat(
+      interpreter.render(
+        "{% set bar = {'my_key': foo} %}{% print (1 + 1 == 3 || bar.my_key.deferred) ~ '.' %}"
+      )
+    )
+      .isEqualTo("{% print (false || good.deferred) ~ '.' %}");
+    assertThat(interpreter.getContext().getDeferredNodes()).isEmpty();
+  }
+
   public static class BadSerialization implements PartiallyDeferredValue {
 
     public String getDeferred() {
@@ -77,6 +174,15 @@ public class PartiallyDeferredValueTest extends BaseInterpretingTest {
       throw new DeferredValueException("entries are deferred");
     }
 
+    @CheckForNull
+    @Override
+    public Object get(@CheckForNull Object key) {
+      if ("deferred".equals(key)) {
+        throw new DeferredValueException("deferred key");
+      }
+      return super.get(key);
+    }
+
     @Override
     public Object getOriginalValue() {
       return null;
@@ -102,7 +208,7 @@ public class PartiallyDeferredValueTest extends BaseInterpretingTest {
     @Override
     public <T extends Appendable & CharSequence> T appendPyishString(T appendable)
       throws IOException {
-      return (T) appendable.append("");
+      throw new DeferredValueException("I'm bad");
     }
   }
 
@@ -126,6 +232,40 @@ public class PartiallyDeferredValueTest extends BaseInterpretingTest {
     public <T extends Appendable & CharSequence> T appendPyishString(T appendable)
       throws IOException {
       return (T) appendable.append("good");
+    }
+  }
+
+  public static class BadEntrySetButPyishSerializable
+    extends PyMap
+    implements PartiallyDeferredValue, PyishSerializable {
+
+    public BadEntrySetButPyishSerializable(Map<String, Object> map) {
+      super(map);
+    }
+
+    @Override
+    public Set<Entry<String, Object>> entrySet() {
+      throw new DeferredValueException("entries are deferred");
+    }
+
+    @CheckForNull
+    @Override
+    public Object get(@CheckForNull Object key) {
+      if ("deferred".equals(key)) {
+        throw new DeferredValueException("deferred key");
+      }
+      return super.get(key);
+    }
+
+    @Override
+    public Object getOriginalValue() {
+      return null;
+    }
+
+    @Override
+    public <T extends Appendable & CharSequence> T appendPyishString(T appendable)
+      throws IOException {
+      return (T) appendable.append("hello");
     }
   }
 }
