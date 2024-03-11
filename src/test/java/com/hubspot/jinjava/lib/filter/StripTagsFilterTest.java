69b8c2d8c5b26b9dfb567694d376bd78785ffba6
Jared Stehler <jstehler@hubspot.com>
2024-03-11 14:25:36 -0400

    upgrade basepom to 59

--- src/test/java/com/hubspot/jinjava/lib/filter/StripTagsFilterTest.java
+++ src/test/java/com/hubspot/jinjava/lib/filter/StripTagsFilterTest.java
@@ -19,7 +19,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StripTagsFilterTest {
