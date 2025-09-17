d2a43f1f0c9c0362673c28872d19a075c8a09b5c
Jack Smith <jasmith@hubspot.com>
2025-09-17 17:44:32 -0400

    Similar to macro functions, for the RenderFilter store a temporary value and throw a DeferredInvocationResolutionException if the render didn't fully complete

--- src/main/java/com/hubspot/jinjava/lib/filter/RenderFilter.java
+++ src/main/java/com/hubspot/jinjava/lib/filter/RenderFilter.java
@@ -4,6 +4,8 @@ import com.hubspot.jinjava.JinjavaConfig;
 import com.hubspot.jinjava.doc.annotations.JinjavaDoc;
 import com.hubspot.jinjava.doc.annotations.JinjavaParam;
 import com.hubspot.jinjava.doc.annotations.JinjavaSnippet;
+import com.hubspot.jinjava.el.ext.DeferredInvocationResolutionException;
+import com.hubspot.jinjava.el.ext.eager.RenderFlatTempVariable;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import java.util.Objects;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -26,16 +28,29 @@ public class RenderFilter implements Filter {
 
   @Override
   public Object filter(Object var, JinjavaInterpreter interpreter, String... args) {
+    int numDeferredTokensStart = interpreter.getContext().getDeferredTokens().size();
+    String result;
     if (args.length > 0) {
       String firstArg = args[0];
-      return interpreter.renderFlat(
-        Objects.toString(var),
-        NumberUtils.toLong(
-          firstArg,
-          JinjavaConfig.newBuilder().build().getMaxOutputSize()
-        )
-      );
+      result =
+        interpreter.renderFlat(
+          Objects.toString(var),
+          NumberUtils.toLong(
+            firstArg,
+            JinjavaConfig.newBuilder().build().getMaxOutputSize()
+          )
+        );
+    } else {
+      result = interpreter.renderFlat(Objects.toString(var));
     }
-    return interpreter.renderFlat(Objects.toString(var));
+    if (interpreter.getContext().getDeferredTokens().size() > numDeferredTokensStart) {
+      String tempVarName = RenderFlatTempVariable.getVarName(result);
+      interpreter
+        .getContext()
+        .getParent()
+        .put(tempVarName, new RenderFlatTempVariable(result));
+      throw new DeferredInvocationResolutionException(tempVarName);
+    }
+    return result;
   }
 }
