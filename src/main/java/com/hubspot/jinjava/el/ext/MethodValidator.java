c0b8795b4bb12e741e5d1a43b2b5f3f0b2852778
jasmith-hs <jasmith@hubspot.com>
2026-03-02 16:36:58 -0500

    Add annotation to MethodValidator

--- src/main/java/com/hubspot/jinjava/el/ext/MethodValidator.java
+++ src/main/java/com/hubspot/jinjava/el/ext/MethodValidator.java
@@ -1,9 +1,10 @@
 package com.hubspot.jinjava.el.ext;
 
 import java.lang.reflect.Method;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public interface MethodValidator {
   @Nullable
-  Method validateMethod(Method m);
+  Method validateMethod(@Nonnull Method m);
 }
